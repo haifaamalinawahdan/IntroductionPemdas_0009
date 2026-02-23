337f0cda7f6a676cc4baedc6ac2f31e59f3fc5bf
haifaamalinawahdan <haifanew3@gmail.com>
2026-02-23 11:56:42 +0700

    Membuat langkah memasukkan variabel

--- app2.cpp
+++ app2.cpp
@@ -6,4 +6,13 @@ int main()
     // langkah 1
     int luas, panjang, lebar;
 
+    // langkah 2
+    cout << "Masukkan Panjang"
+    cin >> panjang;
+    
+    // langkah 3
+    cout << "Masukkan Lebar"
+    cin >> lebar;
+
+
 }
